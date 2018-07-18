082e6d8bac6ed6f0c846a6754265f59de94fc5ac
Aiyoj <xushiqitc@163.com>
2018-07-18 16:42:43 +0800

    Init

--- pytorch_cam/grad_cam.py
+++ pytorch_cam/grad_cam.py
@@ -7,24 +7,52 @@
 # @Software : PyCharm
 
 
-import numpy as np
+import cv2
+import glob
 import torch
+import numpy as np
 from torch.autograd import Variable
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from torchvision.datasets.folder import default_loader
+from pytorch_cam.model import FTNet
 
 
-def preprocess_image(img):
-    means = [0.485, 0.456, 0.406]
-    stds = [0.229, 0.224, 0.225]
+# def preprocess_image(img):
+#     means = [0.485, 0.456, 0.406]
+#     stds = [0.229, 0.224, 0.225]
+#
+#     preprocessed_img = img.copy()[:, :, ::-1]
+#     for i in range(3):
+#         preprocessed_img[:, :, i] = preprocessed_img[:, :, i] - means[i]
+#         preprocessed_img[:, :, i] = preprocessed_img[:, :, i] / stds[i]
+#     preprocessed_img = np.ascontiguousarray(np.transpose(preprocessed_img, (2, 0, 1)))
+#     preprocessed_img = torch.from_numpy(preprocessed_img)
+#     preprocessed_img.unsqueeze_(0)
+#     input = Variable(preprocessed_img, requires_grad=True)
+#     return input
 
-    preprocessed_img = img.copy()[:, :, ::-1]
-    for i in range(3):
-        preprocessed_img[:, :, i] = preprocessed_img[:, :, i] - means[i]
-        preprocessed_img[:, :, i] = preprocessed_img[:, :, i] / stds[i]
-    preprocessed_img = np.ascontiguousarray(np.transpose(preprocessed_img, (2, 0, 1)))
-    preprocessed_img = torch.from_numpy(preprocessed_img)
-    preprocessed_img.unsqueeze_(0)
-    input = Variable(preprocessed_img, requires_grad=True)
-    return input
+
+class MyData(Dataset):
+    def __init__(self, img_path, transformer=None, loader=default_loader):
+        self.img_name = glob.glob('{}/*.png'.format(img_path))
+        self.transformer = transformer
+        self.loader = loader
+
+    def __len__(self):
+        return len(self.img_name)
+
+    def __getitem__(self, item):
+        img_name = self.img_name[item]
+        img = self.loader(img_name)
+
+        if self.transformer is not None:
+            try:
+                img = self.transformer(img)
+            except:
+                print("Cannot transform image: {}".format(img_name))
+        return img_name, img
 
 
 class GradCAM:
@@ -68,7 +96,7 @@ class GradCAM:
         # If None, returns the map for the highest scoring class.
         # Otherwise, targets the requested index.
         if index is None:
-            index = np.argmax(outputs.cpu().data.numpy())
+            index = np.argmax(outputs.cpu().data.numpy(), axis=1)
 
         # 图片数，类别数
         n_sample, n_class = outputs.size()
@@ -107,45 +135,39 @@ class GradCAM:
 
 
 if __name__ == '__main__':
-    import cv2
-    from torchvision import transforms
-    from pytorch_cam.model import FTNet
+    use_cuda = False
+    height, width = 244, 244
 
-    cuda = False
-    device = torch.device('cuda' if cuda and torch.cuda.is_available() else 'cpu')
-    h, w = 244, 244
-    # load img method1
-    # img = cv2.imread('../examples/both.png')[..., ::-1]
-    # img = cv2.resize(img, (w, h)).astype(np.float32)
-    # transformer = transforms.Compose([
-    #     transforms.ToTensor(),
-    #     transforms.Normalize(
-    #         mean=[0.485, 0.456, 0.406],
-    #         std=[0.229, 0.224, 0.225],
-    #     )
-    # ])
-    # x = transformer(img).unsqueeze(0).to(device)
+    transformer = transforms.Compose([
+        transforms.Resize((height, width), interpolation=3),
+        transforms.ToTensor(),
+        transforms.Normalize(
+            mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225],
+        )
+    ])
+    data_set = MyData(img_path='../examples', transformer=transformer)
+    data_loader = DataLoader(dataset=data_set, batch_size=2, shuffle=False, num_workers=2)
+    model = FTNet(1036)
+    grad_cam = GradCAM(model=model, use_cuda=use_cuda)
 
-    # load img method2
-    img = cv2.imread('../examples/both.png', 1)
-    img = cv2.resize(img, (w, h))
-    img = img.astype(np.float32)
-    img /= 255
-    x = preprocess_image(img)
-    # grad_cam 可以同时处理多张图片
-    grad_cam = GradCAM(model=FTNet(100), use_cuda=cuda)
-    cams = grad_cam(x)
+    for data in data_loader:
+        # 一个batch_size大小数目的图片
+        x_name, x = data
+        cams = grad_cam(x)
 
-    for i in range(cams.shape[0]):
-        cam = cv2.resize(cams[i], (w, h))
-        cam = cam - np.min(cam)
-        cam = cam / np.max(cam)
+        for i in range(cams.shape[0]):
+            cam = cv2.resize(cams[i], (width, height))
+            cam = cam - np.min(cam)
+            cam = cam / np.max(cam)
 
-        heatmap = cv2.applyColorMap(np.uint8(255 * cam), cv2.COLORMAP_JET)
+            heatmap = cv2.applyColorMap(np.uint8(255 * cam), cv2.COLORMAP_JET)
 
-        print(heatmap.shape)
-        heatmap = heatmap.astype(np.float32)
-        heatmap /= 255
-        cam = heatmap + img
-        cam = cam / np.max(cam)
-        cv2.imwrite("../examples/cam-both.png", np.uint8(255 * cam))
+            heatmap = heatmap.astype(np.float32)
+            heatmap /= 255
+            cam = heatmap + cv2.resize(cv2.imread(x_name[i]).astype(np.float32), (width, height))/255
+            cam = cam / np.max(cam)
+            split = x_name[i].replace('\\', '/').split('/')
+            pre = '/'.join(split[:-1])
+            sufix = 'cam-{}'.format(split[-1])
+            cv2.imwrite('{}'.format(sufix), np.uint8(255 * cam))
